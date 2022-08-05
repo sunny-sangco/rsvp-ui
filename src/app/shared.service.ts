797b6cea075192408f6e1a1606381dcf4d92dee8
sunny <sunny.sangco@gmail.com>
2022-08-05 16:25:08 +0800

    first commit

---
+++ src/app/shared.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+  readonly APIUrl = "https://localhost:5001/api";
+  constructor(private http: HttpClient) {}
+  getRsvpList(): Observable < any[] > {
+      return this.http.get < any > (this.APIUrl + '/rsvp');
+  }
+  addRsvp(val: any) {
+      return this.http.post(this.APIUrl + '/rsvp', val);
+  }
+ 
+}
