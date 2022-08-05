046fd60b0e16d8d75ab8b6c5370d4e2f09b92b53
sunny <sunny.sangco@gmail.com>
2022-08-05 18:13:59 +0800

    test

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/ui12'));
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/ui12'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/ui12/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-app.get('/*', function(req,res){
-  const fullPath = path.join(__dirname + '/dist/ui12/index.html');
-  console.log(" Fetching from.." + fullPath);
-    res.sendFile(fullPath);
-})
-
-console.log('Server started running..');
